9fb4ca645b3a55e479f3fddd36aedc11d21d5398
Denisa Cuta <denisa1506@yahoo.com>
2023-05-22 11:17:34 +0300

    pokemon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,43 @@ import React, { useState, useEffect } from "react";
 
 function App() {
     const [pokemon, setPokemon] = useState(null);
+    const [location, setLocation] = useState(null);
     const [data, setData] = useState(null);
+  
+  
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/location")
       .then((res) => res.json())
-      .then((data) => {
-        setData(data);
+      .then((data) => { 
+        setLocation(data);
         setPokemon(data);
       });
   }, []);
 
-  return (
-    <div className="App">
   
-    </div>
-  );
+    useEffect(() => {
+      fetch("https://pokeapi.co/api/v2/generation/4/")
+        .then((res) => res.json())
+        .then((data) => { console.log(data);
+          setData(data);
+          setPokemon(data);
+        });
+    }, []);
+
+useEffect(() => {
+  for (let id = 1; id <= 100; id++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+        setPokemon(data);
+      });
+  }
+}, []);
+  
+
+return <div className="App"></div>;
 }
 
 export default App;
