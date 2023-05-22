e0ec9bf70b5e42b526e20aa01dbab5eb37219f7f
Denisa Cuta <denisa1506@yahoo.com>
2023-05-22 10:45:16 +0300

    #

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+    const [pokemon, setPokemon] = useState(null);
+    const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/location")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setPokemon(data);
+      });
+  }, []);
+
   return (
     <div className="App">
   
