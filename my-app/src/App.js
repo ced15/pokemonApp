5f334426020f577980bdb9967f031d7ea1b2d5ed
Ionut2707 <ionut.dima2707@gmail.com>
2023-05-22 13:43:03 +0300

    cleanup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,40 @@
-import './App.css';
 import React, { useState, useEffect } from "react";
 
-
 function App() {
-    const [pokemon, setPokemon] = useState(null);
-  const [location, setLocation] = useState({ });
-    const [data, setData] = useState(null);
-  
-  
+  const [pokemon, setPokemon] = useState(null);
+  const [location, setLocation] = useState({});
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/location")
       .then((res) => res.json())
-      .then((data) => { 
+      .then((data) => {
         console.log(data);
         setLocation(data);
         setPokemon(data);
       });
   }, []);
 
-  
-    useEffect(() => {
-      fetch("https://pokeapi.co/api/v2/generation/4/")
-        .then((res) => res.json())
-        .then((data) => { console.log(data);
-          setData(data);
-          setPokemon(data);
-        });
-    }, []);
-
-useEffect(() => {
-  for (let id = 1; id <= 100; id++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/generation/4/")
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         setData(data);
         setPokemon(data);
       });
-  }
-}, []);
-  
+  }, []);
+
+  useEffect(() => {
+    for (let id = 1; id <= 100; id++) {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data);
+          setPokemon(data);
+        });
+    }
+  }, []);
 
   return (
     <div className="App">
